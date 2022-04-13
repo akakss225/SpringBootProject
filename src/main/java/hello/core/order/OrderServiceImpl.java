e7dd33aca65bfe499d4c4152e1813b90eaa5e212
akakss225 <akakss225@naver.com>
2022-04-13 17:25:32 +0900

    DI - lombok 리팩토링

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,26 +6,32 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
 
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    // 생성자주입 > lombok 버전
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
     // 생성자를 통한 의존성 주입 : 불변 / 필수 의존관계에 사용.
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("memberRepository = " + memberRepository);
-        System.out.println("discountPolicy = " + discountPolicy);
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    // lombok > RequiredArgsConstructor 을 통해 생성가능. > 생략!
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        System.out.println("memberRepository = " + memberRepository);
+//        System.out.println("discountPolicy = " + discountPolicy);
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
+
 
     // 수정자 주입 : setter를 통한 의존성 주입.. 선택 / 변경 가능성이 있는 의존관계에 사용.
 //    private MemberRepository memberRepository;
@@ -41,12 +47,14 @@ public class OrderServiceImpl implements OrderService {
 //        this.discountPolicy = discountPolicy;
 //    }
 
+
     // 필드 주입 : 필드에 직접적으로 의존성을 주입.. 코드는 가장 간결. 외부 데이터 변경이 불가. >> 테스트같은 유연한 코딩 불가.
 //    @Autowired
 //    private MemberRepository memberRepository;
 //
 //    @Autowired
 //    private DiscountPolicy discountPolicy;
+    
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
